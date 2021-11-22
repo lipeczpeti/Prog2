6e765f1f83ae099184da5ed3b698a0747bedd699
Lipécz Péter <lipecz.p@gmail.com>
2021-11-22 14:09:58 +0100

    Update

--- Project02/Assets/Scripts/Projectile.cs
+++ Project02/Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ public class Projectile : MonoBehaviour
     [SerializeField]private float speed;
     private float direction;
     private bool hit;
+    private float lifetime;
 
     private Animator anim;
     private BoxCollider2D boxCollider;
@@ -20,6 +21,9 @@ public class Projectile : MonoBehaviour
          if(hit) return;
          float movementSpeed = speed * Time.deltaTime * direction;
          transform.Translate(movementSpeed, 0, 0);
+
+         lifetime += Time.deltaTime;
+         if (lifetime > 5) gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,11 +34,21 @@ public class Projectile : MonoBehaviour
     }
     public void SetDirection(float _direction)
     {
+        lifetime = 0;
         direction = _direction;
         gameObject.SetActive(true);
         hit = false;
         boxCollider.enabled = true;
 
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
 
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
